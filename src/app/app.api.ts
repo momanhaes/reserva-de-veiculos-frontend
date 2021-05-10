7e729ca084eacc4ccfce168d51839bec8cb9920d
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-10 20:54:43 -0300

    feat: Adiciona variáveis de ambiente para config da API

---
+++ src/app/app.api.ts
@@ -0,0 +1,3 @@
+import { environment } from "../environments/environment";
+
+export const VEHICLES_API = environment.vehiclesAPI;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  vehiclesAPI: "http://localhost:3000"
 };
