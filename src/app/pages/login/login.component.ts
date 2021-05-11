fec27655ae1e5ca826d604f8a2a3949f6e538920
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-11 03:43:57 -0300

    feat: Personaliza login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { APPEARD } from 'src/animations/appeard.animation';
+import { THEME, PARAMS } from 'src/animations/particles.animation';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [APPEARD],
 })
 export class LoginComponent implements OnInit {
+  public state = "ready";
+  public theme = THEME;
+  public params = PARAMS;
+  public isMobile: boolean;
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.router.navigate(["/home"]);
   }
 
 }
