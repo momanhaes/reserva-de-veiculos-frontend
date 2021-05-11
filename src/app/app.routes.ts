283049d9c047dae8c02e7ead30d792ebfd7a6ef8
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-10 21:19:36 -0300

    feat: Atuliza arquivo com novas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+
 import { LoginComponent } from "./pages/login/login.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
 
 export const ROUTES: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
     { path: "login", component: LoginComponent },
+    { path: "home", component: HomeComponent },
+    { path: "**", component: NotFoundComponent },
 ];
