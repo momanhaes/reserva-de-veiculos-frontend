d1721bec09aa590b320186e0a4718591a64acb47
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-12 00:29:58 -0300

    feat: Sweetalert ao vehicle card

--- src/app/components/vehicle-card/vehicle-card.component.ts
+++ src/app/components/vehicle-card/vehicle-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { APPEARD } from 'src/animations/appeard.animation';
 import { IVehicle } from './vehicle.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-vehicle-card',
@@ -20,4 +21,32 @@ export class VehicleCardComponent implements OnInit {
     this.placeholder = "assets/img/placeholder.png";
   }
 
+  rent(vehicle: string): void {
+    Swal.fire({
+      title: `Você tem certeza que deseja reservar o veículo ${vehicle}?`,
+      text: "Você não poderá resevar outro veículo, desde que cancele a reserva deste.",
+      icon: 'warning',
+      background: '#f1f1f1',
+      showCancelButton: true,
+      confirmButtonColor: '#fd5d93',
+      iconColor: '#fd5d93',
+      cancelButtonColor: '#313a46',
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não'
+    })
+      .then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: 'Parabéns!',
+            text: `Você resevou o veículo ${vehicle}.`,
+            icon: 'success',
+            background: '#f1f1f1',
+            iconColor: '#fd5d93',
+            showCancelButton: false,
+            confirmButtonColor: '#fd5d93',
+            confirmButtonText: 'Sim',
+          })
+        }
+      })
+  }
 }
