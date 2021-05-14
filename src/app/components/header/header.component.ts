4ff26076643a8afebc28118fe79d2d281cb62955
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-14 14:41:59 -0300

    fix: Ajusta comportamento responsivo do header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { APPEARD } from 'src/animations/appeard.animation';
 import { UserService } from 'src/app/services/user.service';
 import { IHeader } from './header.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -22,7 +23,22 @@ export class HeaderComponent implements OnInit {
   }
 
   logout() {
-    this.userService.logout();
-    this.router.navigate(['/login']);
+    Swal.fire({
+      title: 'Você escolheu sair.',
+      text: 'Tem certeza?',
+      icon: 'warning',
+      background: '#f1f1f1',
+      showCancelButton: true,
+      confirmButtonColor: '#fd5d93',
+      iconColor: '#fd5d93',
+      cancelButtonColor: '#313a46',
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.userService.logout();
+        this.router.navigate(['/login']);
+      }
+    });
   }
 }
