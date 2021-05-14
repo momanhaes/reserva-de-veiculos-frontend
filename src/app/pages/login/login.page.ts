2e4ba3c63a6e8fdd022165e4d412f42bfda35e95
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-14 00:44:14 -0300

    feat: Insere serviço e fluxo de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 import { APPEARD } from 'src/animations/appeard.animation';
 import { THEME, PARAMS } from 'src/animations/particles.animation';
+import { EMAIL_PATTERN } from 'src/utils/patterns';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-login',
@@ -10,18 +14,35 @@ import { THEME, PARAMS } from 'src/animations/particles.animation';
   animations: [APPEARD],
 })
 export class LoginPage implements OnInit {
-  public state = "ready";
+  public state = 'ready';
   public theme = THEME;
   public params = PARAMS;
-  public isMobile: boolean;
+  public form: FormGroup;
 
-  constructor(private router: Router,) { }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(EMAIL_PATTERN),
+      ]),
+      password: new FormControl('', Validators.required),
+    });
   }
 
   login() {
-    this.router.navigate(["/home"]);
-  }
+    if (this.form.invalid) { return; }
+    const user = this.form.value;
 
+    this.userService.login(user.email, user.password).subscribe(
+      (user) => this.notificationService.notify(`Bem-vindo, ${user.name}!`),
+      (response) => this.notificationService.notify(response.error.error),
+      () => { this.router.navigate(['/home']); }
+    );
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { VEHICLES_API } from '../app.api';
+import { map, tap } from 'rxjs/operators';
+
+export interface IUserInfo {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  public user: IUser;
+
+  constructor(private httpClient: HttpClient) { }
+
+  create(user: IUser): Observable<IUserInfo> {
+    return this.httpClient
+      .post<IUserInfo>(`${VEHICLES_API}/users`, user)
+      .pipe(map((user) => user));
+  }
+
+  login(email: string, password: string): Observable<IUser> {
+    return this.httpClient
+      .post<IUser>(`${VEHICLES_API}/users/login`, { email: email, password: password })
+      .pipe(tap((user) => (this.user = user)));
+  }
+
+  logout() {
+    this.user = undefined;
+  }
+
+  isLoggedIn(): boolean {
+    return this.user !== undefined;
+  }
+}
