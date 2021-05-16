4d7cab1a8beca9dfbc7fb04bc137311fc6c28951
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-16 01:33:22 -0300

    feat: Insere particles na página not-found

--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { APPEARD } from 'src/animations/appeard.animation';
+import { PARAMS, THEME } from 'src/animations/particles.animation';
 
 @Component({
   selector: 'app-not-found',
@@ -11,6 +12,8 @@ import { APPEARD } from 'src/animations/appeard.animation';
 export class NotFoundPage implements OnInit {
   public state = 'ready';
   public path: string;
+  public theme = THEME;
+  public params = PARAMS;
 
   constructor(private router: Router) {}
 
