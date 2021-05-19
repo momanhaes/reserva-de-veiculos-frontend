b29094ccaaf0e5dc58f1cdb057d3b30335458a4e
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-18 22:00:23 -0300

    refactor

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,5 +2,5 @@ export const ALERT_THEME = {
   background: '#f1f1f1',
   iconColor: '#7E87FF',
   confirmButtonColor: '#7E87FF',
-  cancelButtonColor: '#1f272e',
+  cancelButtonColor: '#313a46',
 };
